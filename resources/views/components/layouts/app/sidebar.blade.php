63fe75f63c6bdb481faded2829562dc8a077741e
Cafer Mert Kalay <113070286+MertKx@users.noreply.github.com>
2025-07-23 16:14:25 +0300

    changed sidebar hello writing. Radio button will be added

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -12,21 +12,15 @@
             </a>
 
             <flux:navlist variant="outline">
-                <flux:navlist.group :heading="__('Platform')" class="grid">
-                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                <flux:navlist.group class="grid">
+                    <flux:navlist.item icon="book-open-text" :href="route('books.index')" :current="request()->routeIs('books.index')" wire:navigate>{{ __('Click for books') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
             <flux:spacer />
 
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-                </flux:navlist.item>
-
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
-                </flux:navlist.item>
+                Hello !
             </flux:navlist>
 
             <!-- Desktop User Menu -->
